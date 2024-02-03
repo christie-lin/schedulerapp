18ecdeb02af781c326e4814d2d9294ea598f0f43
nhinhiii <158134095+nhinhiii@users.noreply.github.com>
2024-02-03 11:40:45 -0500

    Update MainActivity.java
    This is the main class for stories 1 and 3. I may come back to update after I got an idea on how to put the time as a reminder

--- SchedulerApp/app/src/main/java/com/example/schedulerapp/MainActivity.java
+++ SchedulerApp/app/src/main/java/com/example/schedulerapp/MainActivity.java
@@ -1,64 +1,62 @@
-package com.example.schedulerapp;
+package com.example.scheduleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import com.example.classschedule_test.classInfo;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.view.LayoutInflater;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
-import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link activity_main#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class activity_main extends Fragment {
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.example.classschedule_test.databinding.ActivityMainBinding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import android.view.Menu;
+import android.view.MenuItem;
 
-    public activity_main() {
-        // Required empty public constructor
-    }
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment activity_main.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static activity_main newInstance(String param1, String param2) {
-        activity_main fragment = new activity_main();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.activity_main);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_main, container, false);
+        MaterialButton add = findViewById(R.id.infoButton);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, classInfo.class));
+            }
+        });
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<classInfo> noteList = realm.where(classInfo.class).sort("createTime", Sort.DESCENDING).findAll();
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Apdater myAdapter = new Apdater(getApplicationContext(), noteList);
+        recyclerView.setAdapter(myAdapter);
+        noteList.addChangeListener(new RealmChangeListener<RealmResults<classInfo>>() {
+            @Override
+            public void onChange(RealmResults<classInfo> classInfos) {
+                myAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
